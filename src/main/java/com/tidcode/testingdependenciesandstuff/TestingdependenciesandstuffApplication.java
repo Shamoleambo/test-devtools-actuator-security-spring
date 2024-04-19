371c480d3e26318ce412cc76994940db6bb4da0e
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-04-19 09:33:43 -0300

    first commit

---
+++ src/main/java/com/tidcode/testingdependenciesandstuff/TestingdependenciesandstuffApplication.java
@@ -0,0 +1,13 @@
+package com.tidcode.testingdependenciesandstuff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingdependenciesandstuffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestingdependenciesandstuffApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tidcode/testingdependenciesandstuff/TestingdependenciesandstuffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tidcode.testingdependenciesandstuff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingdependenciesandstuffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
